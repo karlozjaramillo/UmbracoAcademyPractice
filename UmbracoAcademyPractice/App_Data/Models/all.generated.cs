fae1db2586c58129ab0fa99bffd65166a85a0082
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2021-10-21 14:45:00 -0500

    Se crea macro About, se llama en AboutUs.cshtml y se modifica .gitignore

--- UmbracoAcademyPractice/App_Data/Models/all.generated.cs
+++ UmbracoAcademyPractice/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "15ee3dfa0b50b5ad")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
