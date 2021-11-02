4b3adcd8e8822745e72dae5ea768c9a3cbd1566a
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2021-11-02 12:14:49 -0500

    Se agrega controlador para AboutUs y su respectiva vista y se elimina la vista AboutUs.cshtml

---
+++ UmbracoAcademyPractice/Controllers/AboutUsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoAcademyPractice.Controllers
+{
+    public class AboutUsController : SurfaceController, IRenderMvcController
+    {
+        public ActionResult Index(RenderModel model)
+        {
+            return View(model);
+        }
+    }
+}
